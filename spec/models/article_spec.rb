b2ecd0b08cf9369d2688569d28337418734d045a
ShavdatuashviliSandro <sandro.shav@gmail.com>
2022-05-25 16:36:30 +0400

    rspec added

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  describe '#validations' do
+    let(:article) { FactoryBot.build(:article) }
+
+    it 'tests if article is valid' do
+      expect(article).to be_valid
+    end
+
+    it 'has an invalid title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
+
+    it 'should has a has valid content' do
+      article.content = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:content]).to include("can't be blank")
+    end
+
+    it 'should has a has valid slug' do
+      article.slug = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:slug]).to include("can't be blank")
+    end
+
+  end
+
+end
